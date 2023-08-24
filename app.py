9828f261703367ebc03652cf7db1d1b9c17ed6aa
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-08-24 16:54:16 -0400

    Relationship one-to-one, one-to-many, and many-to-many

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import datetime
 from flask import Flask, request, jsonify
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import db, Note
+from models import db, Note, User, Profile, Category
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -21,7 +21,6 @@ CORS(app)
 @app.route('/')
 def main():
     return jsonify({ "status": "OK"}), 200
-
 
 @app.route('/api/notes', methods=['GET'])
 def list_notes():
@@ -42,12 +41,22 @@ def read_note(id):
 def create_note():
     
     body = request.json.get("body")
+    categories = request.json.get("categories")
     
     if not body:
         return jsonify({"msg": "Body is required!"}), 400
     
     note = Note()
     note.body = body
+    note.user_id = 1
+    
+    for category_id in categories:
+        category = Category.query.get(category_id)
+        
+        if not category in note.categories:
+            note.categories.append(category)
+    
+    
     note.save() # llamamos a la funcion save para crear el nuevo objeto note
     
     #db.session.add(note)
@@ -55,12 +64,11 @@ def create_note():
     
     return jsonify(note.serialize()), 201
     
-    
-
 @app.route('/api/notes/<int:id>', methods=['PUT'])
 def update_note(id):
     
     body = request.json.get("body")
+    categories = request.json.get("categories")
     
     if not body:
         return jsonify({"msg": "Body is required!"}), 400
@@ -70,19 +78,35 @@ def update_note(id):
         return jsonify({ "msg": f"Note with id {id} not found!"}), 404 
     
     note.body = body
+    
+    for category_id in categories:
+        category = Category.query.get(category_id)
+        
+        if not category in note.categories:
+            note.categories.append(category)
+    
+    
     note.update() # llamamos a la funcion update para guardar los cambios del objeto note
     
     #db.session.commit() 
     
     return jsonify(note.serialize()), 200
 
+@app.route('/api/notes', methods=['DELETE'])
 @app.route('/api/notes/<int:id>', methods=['DELETE'])
-def delete_note(id):
-    note = Note.query.get(id)
-    if not note:
-        return jsonify({ "msg": f"Note with id {id} not found!"}), 404
-
-    note.delete() # llamamos a la function delete del objeto note para eliminar
+def delete_note(id = None):
+    
+    if id is not None:
+        note = Note.query.get(id)
+        if not note:
+            return jsonify({ "msg": f"Note with id {id} not found!"}), 404
+        note.delete() # llamamos a la function delete del objeto note para eliminar
+        
+    else:
+        #notes = Note.query.all()
+        notes = Note.query.filter_by(user_id=1)
+        for note in notes:
+            note.delete()
 
     return jsonify({ "success": "Note deleted!"}), 200
 
@@ -106,6 +130,66 @@ def search_notes():
     
     return jsonify({ "search": search, "results": notes }), 200
 
+@app.route('/api/test')
+def test_models():
+    
+    user = User()
+    user.username = "lrodriguez@4geeks.co"
+    
+    """
+    user.save()
+    profile = Profile()
+    profile.user_id = user.id
+    profile.save()
+    """
+    profile = Profile()
+    user.profile = profile
+    #user.save()
+    
+    
+    #user = User.query.get(4)
+    #user.profile.biography = "Soy profesor"
+    #user.profile.twitter = "luisjrodriguezo"
+    #user.update()
+    
+    #user = User.query.get(3)
+    #user.profile.delete()
+    #user.delete()
+    
+    for i in range(11):
+        note = Note()
+        note.body = f"This is my note {i+1}"
+        #note.user_id = user.id 
+        #note.save()
+        user.notes.append(note)
+    
+    user.save()
+    
+    return jsonify({"message": "check the database!"}), 200
+    
+
+@app.route('/api/test/categories')
+def load_categories():
+    
+    
+    categories = ["Technologies", "Health", "Nationals", "Internationals", "World"]
+    
+    for cat in categories:
+        category = Category()
+        category.name = cat
+        category.save()
+        
+    return jsonify({"message": "Categories loaded!"}), 200
+
+@app.route('/api/user/<int:id>')
+def read_user_by_id(id):
+    
+    user = User.query.get(id)
+    
+    if not user:
+        return jsonify({ "msg": f"User with id {id} not found!"}), 404 
+    
+    return jsonify(user.serialize()), 200
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,19 +1,58 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class Note(db.Model):
-    __tablename__ = 'notes'
+
+notes_categories = db.Table(
+    'notes_categories',
+    db.Column('note_id', db.Integer, db.ForeignKey('notes.id', ondelete="CASCADE"), nullable=False, primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('categories.id', ondelete="CASCADE"), nullable=False, primary_key=True)
+)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(200), nullable=False, index=True)
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    
+    profile = db.relationship('Profile', cascade="all, delete", backref='user', uselist=False)
+    notes = db.relationship('Note', cascade="all, delete", backref='user')
     
     def serialize(self):
         return {
             "id": self.id,
-            "body": self.body,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at,
+            "username": self.username,
+            "biography": self.profile.biography,
+            "notes": [note.serialize() for note in self.notes]
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit() 
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit() 
+
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    biography = db.Column(db.String(200), default="")
+    facebook = db.Column(db.String(200), default="")
+    twitter = db.Column(db.String(200), default="")
+    instagram = db.Column(db.String(200), default="")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "biography": self.biography,
+            "facebook": self.facebook,
+            "twitter": self.twitter,
+            "instagram": self.instagram,
         }
         
     def save(self):
@@ -26,3 +65,62 @@ class Note(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit() 
+        
+
+class Note(db.Model):
+    __tablename__ = 'notes'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(200), nullable=False, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    
+    categories = db.relationship('Category', secondary=notes_categories)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "body": self.body,
+            "user_id": self.user_id,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+            "username": self.user.username,
+            "categories": [category.serialize() for category in self.categories]
+        }
+        
+    def save(self):
+        db.session.add(self)
+        db.session.commit() 
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit() 
+        
+
+class Category(db.Model):
+    __tablename__ = "categories"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    
+    notes = db.relationship('Note', secondary=notes_categories)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+        
+    def save(self):
+        db.session.add(self)
+        db.session.commit() 
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit() 
+        
